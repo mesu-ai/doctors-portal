8181c6132858c3c773caac1b099eae724e9f6907
mesu-ai <mesu.s.ss@gmail.com>
2021-11-05 04:05:14 +0600

    appintment booking modal

--- src/pages/appointment/bookingCard/BookingCard.js
+++ src/pages/appointment/bookingCard/BookingCard.js
@@ -1,8 +1,17 @@
 import { Button, Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import BookingModal from '../bookingModal/BookingModal';
 
 const BookingCard = ({booking}) => {
+
     const {name,time,space}=booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const bookingModalOpen = () => setOpenBooking(true);
+    const bookingModalClose = () => setOpenBooking(false);
+  
+
+
+
     return (
         <Grid item xs={12} sm={6} md={6} lg={4}>
         <Paper sx={{padding:5}} elevation={3}>
@@ -16,7 +25,9 @@ const BookingCard = ({booking}) => {
         {space}
       </Typography>
 
-          <Button variant="contained">Book Appointment</Button>
+          <Button onClick={bookingModalOpen} variant="contained">Book Appointment</Button>
+
+          <BookingModal bookingModalOpen={bookingModalOpen} bookingModalClose={bookingModalClose} openBooking={openBooking} booking={booking}></BookingModal>
 
         
 
---
+++ src/pages/appointment/bookingModal/BookingModal.js
@@ -0,0 +1,81 @@
+import { Backdrop, Button, Fade, Modal, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'white',  
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const BookingModal = ({bookingModalOpen,bookingModalClose,openBooking,booking}) => {
+
+    const {name,time,space}=booking;
+
+    const bookingHandle =(e)=>{
+
+        bookingModalClose();
+
+        e.preventDefault();
+    }
+    
+     return (
+        <div>
+     
+      <Modal 
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={openBooking}
+        onClose={bookingModalClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openBooking}>
+          <Box  sx={style}>
+
+          <Typography sx={{mb:3,textAlign:'center',color:'info.main'}} id="transition-modal-title" variant="h6" component="h2">
+              {name}
+            </Typography>
+
+            <form onSubmit={bookingHandle}>
+
+            <TextField sx={{mb:2}} fullWidth label="Name" id="fullWidth" />
+            <TextField sx={{mb:2}} fullWidth label="Email" id="fullWidth" />
+            <TextField sx={{mb:2}} fullWidth label="Phone" id="fullWidth" />
+            <TextField sx={{mb:2}} fullWidth label="Time" id="fullWidth" />
+            <TextField sx={{mb:3}} fullWidth label="date" id="fullWidth" />
+
+            <Button sx={{}} variant="contained" type="submit">SEND</Button>
+            
+
+
+
+
+            </form>
+
+
+          
+    
+            {/* <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography> */}
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+    );
+};
+
+export default BookingModal;
